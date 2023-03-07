44745428ad2ccd23c36e41b1e9efdcb26d58d157
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-03-07 04:19:23 +0000

    [update]投稿機能一通り記述（エラー出る）

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,18 +6,42 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:tag).join(',')
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    place_present = Place.where(place_name: :place)
+    if place_present.blank?
+      plase = Place.new(place_params)
+      plase.place_name = params[:post][:place]
+    end
+    @post.place_id = place.id
+    #タグの保存処理
+    tag_list = params[:post][:tag].delete(' ').delete('　').split(',')#送られたtag情報を「,」で区切ってスペースを削除
+    if @post.update(post_params)
+      @post.save_tags(tag_list) #save_tagsメソッドを実行（モデルに記載）
+      redirect_to root_path, notice: "編集が完了しました"
+    end
   end
 
   def create
     @post = Post.new(post_params)
     @post.customer_id = current_customer.id
-    #場所が存在すればidを入れる。無ければ新規作成。
+    #場所の保存処理
     place_present = Place.where(place_name: :place)
     if place_present.blank?
-      @plase = Place.new(place_params)
-      @plase.place_name = :place
+      place = Place.new
+      place.place_name = params[:post][:place]
     end
     @post.place_id = place.id
+    #タグの保存処理
+    tag_list = params[:post][:tag].delete(' ').delete('　').split(',')#送られたtag情報を「,」で区切ってスペースを削除
+    if @post.save
+      @post.save_tags(tag_list) #save_tagsメソッドを実行（モデルに記載）
+      redirect_to root_path, notice: "投稿が完了しました"
+    end
   end
 
   private
