2525ef892d58fd530a8b0842759ad6569ec829e9
Pirozhki <h.kanazawa111@gmail.com>
2023-03-11 05:35:45 +0000

    [add]admin側ユーザーの編集機能

--- app/controllers/admin/customer_controller.rb
+++ app/controllers/admin/customer_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CustomerController < ApplicationController
+  before_action
   def index
     @customers = Customer.all
   end
@@ -8,5 +9,20 @@ class Admin::CustomerController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "更新に成功しました"
+    else
+      render 'admin/customer/edit', notice: "更新に失敗しました"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:is_deleted)
   end
 end
