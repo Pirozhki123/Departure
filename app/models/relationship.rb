5727b5a489bef5f28c9de5d911cb641b66267f2c
Pirozhki <h.kanazawa111@gmail.com>
2023-03-13 08:38:29 +0000

    [fix]relations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,5 @@
 class Relationship < ApplicationRecord
   has_many :notifications, dependent: :destroy
-  belongs_to :follower, class_name: "Customer"#, optional: true
-  belongs_to :followed, class_name: "Customer"#, optional: true
+  belongs_to :follower, class_name: "Customer"
+  belongs_to :followed, class_name: "Customer"
 end
