7e12a0b95b9e13c71e0c5e3a8087c95e29238f33
Pirozhki <h.kanazawa111@gmail.com>
2023-03-20 01:47:30 +0000

    [fix]画像のサイズ統一

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,7 +6,10 @@ class Public::HomesController < ApplicationController
     customer_ids = current_customer.followings.pluck(:id) #フォローしているユーザーidを取得
     customer_ids.push(current_customer.id) #customer_idsにcurrent_customer.idを追加
     @posts = Post.where(customer_id: customer_ids).order(created_at: :desc) #取得したユーザーidの投稿を取得
-    @tag_list = @post.tags.pluck(:tag).join(',')
+
+    not_my_post = Post.where.not(customer_id: current_customer.id)
+    @recommend = not_my_post.where( 'id >= ?', rand(not_my_post.first.id..not_my_post.last.id) ).limit(2)
+
   end
 
   def about
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -21,23 +21,11 @@ class Public::PostsController < ApplicationController
   end
 
   def update
-    place_name = params[:post][:place]
-    place_present = Place.where(place_name: place_name)
-    #場所の保存処理
-    if place_present.blank?
-      @post.place = Place.new
-      @post.place.place_name = place_name
-      @post.place.save
-      @post.place_id = @post.place.id
-    else
-      place = Place.where(place_name: place_name).order('created_at DESC').first
-      @post.place_id = place.id
-    end
-    #タグの保存処理
+    @post = Post.find(params[:id])
     tag_list = params[:post][:tag].delete(' ').delete('　').split(',')#送られたtag情報を「,」で区切ってスペースを削除
     if @post.update(post_params)
       @post.save_tags(tag_list) #save_tagsメソッドを実行（モデルに記載）
-      redirect_to root_path, notice: "投稿が完了しました"
+      redirect_to root_path, notice: "編集が完了しました"
     else
       render 'public/homes/top'
     end
