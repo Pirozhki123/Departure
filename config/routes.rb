e552642d62d68afbad4cbfe42d0232bdddf1ecd7
Pirozhki <h.kanazawa111@gmail.com>
2023-05-09 06:04:42 +0000

    [updata]リダイレクトループが発生していた問題を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
   root to: "public/homes#top"
   get "about" => "public/homes#about"
 
@@ -27,12 +35,6 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
